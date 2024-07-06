771b9e08ed793696d6554a3eeb2894c21279d590
j.choriyev <j.choriyev@uic.group>
2024-07-06 21:55:36 +0500

    initial commit

---
+++ tgbot/bot/app.py
@@ -0,0 +1,59 @@
+import asyncio
+import orjson
+
+from django.conf import settings
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from tgbot.bot import handlers
+
+
+def setup_handlers(dp: Dispatcher) -> None:
+    dp.include_router(handlers.users.prepare_router())
+
+
+def setup_middlewares(dp: Dispatcher) -> None:
+    pass
+
+
+async def setup_aiogram(dp: Dispatcher) -> None:
+    setup_handlers(dp)
+    setup_middlewares(dp)
+
+
+async def aiogram_on_startup_polling(dispatcher: Dispatcher, bot: Bot) -> None:
+    await setup_aiogram(dispatcher)
+
+
+async def aiogram_on_shutdown_polling(dispatcher: Dispatcher, bot: Bot) -> None:
+    await bot.session.close()
+    await dispatcher.storage.close()
+
+
+def main() -> None:
+    session = AiohttpSession(
+        json_loads=orjson.loads,
+    )
+
+    bot = Bot(
+        token=settings.API_TOKEN,
+        session=session,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    storage = MemoryStorage()
+
+    dp = Dispatcher(
+        storage=storage,
+    )
+    dp.startup.register(aiogram_on_startup_polling)
+    dp.shutdown.register(aiogram_on_shutdown_polling)
+    asyncio.run(dp.start_polling(bot))
+
+
+# if __name__ == "__main__":
+#     main()
